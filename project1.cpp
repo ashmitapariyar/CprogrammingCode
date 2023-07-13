8a9b796df70dd23fe2145c78931377341c5f3c54
ashmitapariyar <ashmitapariyar61@gmail.com>
2023-07-13 07:21:18 +0545

    Updated by Ashmita Pariyar July 13 7:21 Am

---
+++ project1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int rollNumber=10;
+	char gender='F';
+	long phoneNumber=98194367;
+	char email[20]="ashmita@gmail.com";
+	char name[20]="Ashmita";
+	printf("My CV");
+	printf("\%n");
+	printf("rollNumber=%d", rollNumber);
+	printf("\%n");
+	printf("gender=%c", gender);
+	printf("\%n");
+	printf("phoneNumber=%ld", phoneNumber);
+	printf("\%n");
+	printf("email=%s", email);
+	printf("\%n");
+	printf("name=%s", name);
+	
+	return 0;
+	
+	
+	
+}
